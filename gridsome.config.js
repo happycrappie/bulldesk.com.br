5d58f24837c52c0d6019d3a3f4b5a5d8a9a7f306
Gabriel Koerich <gabriel@bulldesk.com.br>
2019-06-04 19:03:11 -0300

    Start gridsome project

---
+++ gridsome.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteName: 'Bulldesk',
+  siteDescription: 'Com o Bulldesk você só precisa do Bulldesk.',
+  siteUrl: 'https://bulldesk.com.br',
+  plugins: [
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'x123'
+      }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000
+      }
+    }
+  ],
+  css: {
+    loaderOptions: {
+      scss: {
+      }
+    }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('pug')
+      .test(/\.pug$/)
+      .use('pug-plain-loader')
+      .loader('pug-plain-loader')
+  }
+}
---
+++ src/layouts/Default.vue
@@ -0,0 +1,42 @@
+<style lang="sass">
+  body
+    font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif
+    margin: 0
+    padding: 0
+    line-height: 1.5
+
+  .layout
+    max-width: 760px
+    margin: 0 auto
+    padding-left: 20px
+    padding-right: 20px
+
+  .header
+    display: flex
+    justify-content: space-between
+    align-items: center
+    margin-bottom: 20px
+    height: 80px
+
+  .nav__link
+    margin-left: 20px
+</style>
+
+<template lang="pug">
+  .layout
+    header.header
+      strong
+        g-link(to="/") {{ $static.metaData.siteName }}
+      nav.nav
+        g-link.nav__link(to="/") Home
+        g-link.nav__link(to="/about") About
+    <slot/>
+</template>
+
+<static-query>
+  query {
+    metaData {
+      siteName
+    }
+  }
+</static-query>
