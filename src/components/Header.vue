1e9c6d28d8488ca724ae7cd1e4ab682627876d6b
Gabriel Koerich <gabriel@bulldesk.com.br>
2019-06-06 00:07:54 -0300

    Split components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -23,9 +23,6 @@
     .logo
       width: 140px
 
-    nav
-      margin-bottom: 1rem
-
     .hero
       height: 90%
       width: 40%
@@ -43,19 +40,7 @@
 <template lang="pug">
   header.d-flex
     .container
-      b-navbar.d-flex.p-0(toggleable="md" type="light")
-        b-navbar-brand
-          g-link(to="/")
-            g-image.logo(src="../assets/images/logo.png" alt="Bulldesk")
-        b-navbar-toggle(target="collapse")
-        b-collapse#collapse(is-nav)
-          b-navbar-nav.ml-auto
-            b-nav-item(href="/") Home
-            b-nav-item(href="/") Pra quem é
-            b-nav-item(href="/cases") Cases
-            b-nav-item(href="/") Planos
-            b-nav-item(href="https://app.bulldesk.com.br" target="_blank") Login
-            b-nav-item(href="/cadastro") Teste grátis
+      Nav
 
       .hero.d-flex.align-items-center
         div
@@ -64,3 +49,13 @@
             span.dot.green
           p Ferramenta de controle vendas, gerenciamento de contatos e automação de relacionamento.
 </template>
+
+<script>
+  import Nav from './Nav'
+
+  export default {
+    components: {
+      Nav,
+    },
+  }
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,20 @@
+<style lang="sass" scoped>
+  nav
+    margin-bottom: 1rem
+</style>
+
+<template lang="pug">
+  b-navbar.d-flex.p-0(toggleable="md" type="light")
+    b-navbar-brand
+      g-link(to="/")
+        g-image.logo(src="../assets/images/logo.png" alt="Bulldesk")
+    b-navbar-toggle(target="collapse")
+    b-collapse#collapse(is-nav)
+      b-navbar-nav.ml-auto
+        b-nav-item(href="/") Home
+        b-nav-item(href="/") Pra quem é
+        b-nav-item(href="/cases") Cases
+        b-nav-item(href="/") Planos
+        b-nav-item(href="https://app.bulldesk.com.br" target="_blank") Login
+        b-nav-item(href="/cadastro") Teste grátis
+</template>
