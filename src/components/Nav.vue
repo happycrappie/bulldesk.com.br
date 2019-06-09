038456285d8b6463815f8f6cc2468a7305f8688b
Gabriel Koerich <gabriel@bulldesk.com.br>
2019-06-09 01:39:01 -0300

    Just shrink when lg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -12,6 +12,7 @@
     .nav-item
       &.trial
         .nav-link
+          text-align: center
           padding: 8px 25px
           background: $green
           color: $black
@@ -23,18 +24,18 @@
 </style>
 
 <template lang="pug">
-  b-navbar.d-flex(toggleable="md" :type="type")
+  b-navbar.d-flex(toggleable="lg" :type="type")
     b-navbar-brand
       g-link(to="/")
         g-image.logo(src="../assets/images/logo.png" alt="Bulldesk")
     b-navbar-toggle(target="collapse")
     b-collapse#collapse(is-nav)
       b-navbar-nav.ml-auto
-        b-nav-item(to="/") O que faz.
-        b-nav-item(to="/") Pra quem é.
-        b-nav-item(to="/cases") Conheça.
-        b-nav-item(to="/") Planos.
-        b-nav-item.font-weight-bold(href="https://app.bulldesk.com.br" target="_blank") Login.
+        b-nav-item(to="/") O que faz
+        b-nav-item(to="/") Pra quem é
+        b-nav-item(to="/cases") Conheça
+        b-nav-item(to="/") Planos
+        b-nav-item(href="https://app.bulldesk.com.br" target="_blank") Login
         b-nav-item.font-weight-bold.trial(to="/cadastro") Teste grátis
 </template>
 
