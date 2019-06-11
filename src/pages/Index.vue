bc2942e8d0a3275ab499ddea07ab7a67d6eca7f4
Arthur Borba <artsborba@gmail.com>
2019-06-11 16:24:18 -0300

    insert your email here

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -40,6 +40,42 @@
         font-size: 1.1rem
         font-weight: 100
         max-width: 420px
+
+      .email-group
+        text-align: left
+        margin-top: 34px
+        width: 381px
+
+      .email-input
+        box-shadow: 0 10px 20px rgba(165, 131, 224, 0.18)
+        background-color: $white
+        border-radius: 30px
+        padding-left: 30px
+        font-size: 14px
+        height: 68px
+
+      .email-button
+        width: 56px
+        height: 56px
+        border: 0px
+        border-radius: 28px
+        margin-left: -63px
+        margin-top: 6px
+        z-index: 999
+        background-color: $green-light
+
+        img
+          margin-left: 3px
+
+      .email-paragraph
+        width: 207px
+        height: 53px
+        color: $gray-light
+        font-size: 10px
+        font-weight: 400
+        letter-spacing: 0.2px
+        line-height: 20px
+        margin: 21px 144px 0px 30px
 </style>
 
 <template lang="pug">
@@ -53,6 +89,17 @@
             h1.font-weight-bold Inteligência comercial de forma simples
               span.dot.green
             p Ferramenta de controle vendas, gerenciamento de contatos e automação de relacionamento.
+            b-input-group.email-group
+              b-form-input.email-input(placeholder='Insira seu email aqui' v-model='email')
+              b-button.email-button
+                g-image(src='~/assets/icons/play-button.svg')
+            p.email-paragraph
+              | Experimente de graça.
+              br
+              | Nenhum cartão de crédito é necessário.
+              br
+              | Configuração simples e rápida
+
 </template>
 
 <script>
