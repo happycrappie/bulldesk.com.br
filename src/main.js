b9cf572228bfa155d9c2bc578149d54d7f4054ef
Gabriel Koerich <gabriel@bulldesk.com.br>
2019-06-25 14:31:28 -0300

    pt-br lang

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import { TooltipPlugin } from 'bootstrap-vue'
 export default function (Vue, { router, head, isClient }) {
   Vue.use(TooltipPlugin);
 
+  head.htmlAttrs = { lang: 'pt-br' }
+
   head.link.push({
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap'
