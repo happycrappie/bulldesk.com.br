14f2155d91ec25cbfec2a74ac99794d8049b1c2a
Gabriel Koerich <gabriel@bulldesk.com.br>
2019-06-27 15:45:59 -0300

    Meta stuff

--- src/main.js
+++ src/main.js
@@ -9,10 +9,17 @@ export default function (Vue, { router, head, isClient }) {
   Vue.use(ButtonPlugin);
   Vue.use(TooltipPlugin);
 
+  const description = 'Com o Bulldesk você tem automação de marketing e CRM em um lugar só. Faça suas campanhas de Marketing Digital em pouco tempo e acelere ao máximo seu processo de vendas. Menos gastos, Mais conversões. Menos softwares, mais performance.';
+
   head.htmlAttrs = { lang: 'pt-br' }
+
+  head.meta.push({ name: 'og:type', content: 'website' });
+  head.meta.push({ name: 'description', content: description });
+  head.meta.push({ name: 'og:description', content: description });
+  head.meta.push({ name: 'keywords', content: 'Automação de Marketing, CRM' });
 
   head.link.push({
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap'
-  })
+  });
 }
