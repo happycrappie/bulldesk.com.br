a307a53c3d2a1b9cd274258f06a4ae58561f9001
Bruno de Oliveira <contato@bruno.cafe>
2019-07-24 17:27:33 -0300

    Blog Category

---
+++ src/templates/WordPressCategory.vue
@@ -0,0 +1,201 @@
+<style lang="sass" scoped>
+  @import 'node_modules/include-media/dist/_include-media.scss'
+  @import '../assets/styles/_variables.scss'
+
+  header
+    position: relative
+    z-index: 4
+
+  .hero
+    position: relative
+    margin-bottom: 100px
+    padding-top: 100px
+
+    &:before
+      content: ""
+      position: absolute
+      top: -72px
+      left: 0
+      z-index: -1
+      width: 100vw
+      height: 883px
+      background: bottom center no-repeat url(~@/assets/images/blog-bg-hero.png)
+      background-size: cover
+
+    h1
+      font-size: 3.9375rem
+      font-weight: bold
+      line-height: 0.87
+      color: $gray-dark
+
+  .section-a
+    .row
+      counter-reset: posts
+
+      .col
+        margin-bottom: 30px
+
+        article
+          display: flex
+          flex-direction: column
+          height: 495px
+          color: $gray-dark
+          background: $white-gray
+          box-shadow: 0 0 0px 0 rgba(74, 44, 126, 0.37)
+          transition: all 0.1s linear
+          transform: translateZ(0)
+
+          &:hover
+            box-shadow: 0 0 20px 0 rgba(74, 44, 126, 0.37)
+
+          > *
+            padding-left: 30px
+            padding-right: 30px
+
+          .article-featured
+            flex: 0 0 auto
+            width: 100%
+            height: 247px
+            padding-left: 0
+            padding-right: 0
+            overflow: hidden
+            background: #cccccc
+
+            a
+              display: block
+              height: 100%
+
+            img
+              display: block
+              width: 100%
+              height: 100%
+              object-fit: cover
+
+          small
+            margin-top: 40px
+            margin-bottom: 10px
+            font-size: 0.625rem
+            font-weight: bold
+            color: inherit
+
+          h2
+            flex-grow: 1
+            margin-bottom: 20px
+            font-size: 1.125rem
+            line-height: 1.2
+
+          a
+            color: inherit
+
+            &[href]:hover
+              text-decoration: none
+              color: $purple
+
+          .article-body
+            display: none
+            font-size: 0.75rem
+            line-height: 1.67
+            color: $white-gray
+
+          time
+            margin-bottom: 40px
+            font-size: 0.75rem
+
+</style>
+<style lang="sass">
+  .section-a
+    .row
+      .col
+        article
+          .article-body
+            p
+              margin-bottom: 0
+
+</style>
+
+<template lang="pug">
+  Layout
+    header.d-flex
+      .container
+        NavBlog
+
+    section.hero
+      .container
+        .row
+          h1.col-lg-10.col-md-12 Conteúdo inteligente feito pra você
+            span.dot.green
+
+    section.section-a
+      .container
+        .row
+          .col.col-sm-12.col-lg-6.col-xl-4(v-for="(edge, index) in $page.category.belongsTo.edges", :style="{order:index}")
+            article
+              div.article-featured
+                a(:href="edge.node.path").d-flex.justify-content-center.align-items-center
+                  g-image(:src="edge.node.featuredMedia.sourceUrl", v-if="edge.node.featuredMedia")
+              small
+                a {{edge.node.categories[0].title}}
+              h2
+                a(:href="edge.node.path") {{ edge.node.title }}
+              div.article-body(v-html="edge.node.excerpt")
+              time {{ edge.node.date | date }}
+
+
+</template>
+
+<page-query>
+query Category ($path: String, $page: Int) {
+  category: wordPressCategory (path: $path) {
+    title
+    belongsTo (page: $page, perPage: 10) @paginate {
+      pageInfo {
+        totalPages
+        currentPage
+      }
+      edges {
+        node {
+          ... on WordPressPost {
+            id
+            title
+            path
+            excerpt
+            slug
+            categories {
+              title
+              path
+            }
+            date
+            featuredMedia {
+              id
+              sourceUrl
+            }
+        	}
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+  import Layout from '../layouts/Default'
+  import NavBlog from '../components/NavBlog'
+  import EmailInput from '../components/EmailInput'
+  import helpers from '../shared/helpers'
+
+
+  export default {
+    filters: {
+      date: helpers.convertDate
+    },
+    components: {
+      Layout,
+      NavBlog,
+      EmailInput,
+    },
+
+    metaInfo: {
+      title: 'Blog',
+    },
+  }
+</script>
